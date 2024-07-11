25d742ed082d0912b4b373e14c7f1140a15b7f77
Jeonghoon2 <ukidd12@naver.com>
2024-07-11 10:08:53 +0900

    sudden 스크립트 수정

--- sudden.sh
+++ sudden.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#/bin/bash
 
-FILE=./_SUDDEN
+FILE="_SUDDEN"
 
 while true
 do
-        if [[ -f "맥라렌.html" ]]; then
-                echo "check"
-                sleep 3
+        if [[ -f "$FILE" ]]; then
+                echo "SUDDEN!"
+	else
+		clear
         fi
+	sleep 3
+
 done
 
